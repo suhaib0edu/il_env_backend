a537c4a9b28a0bc435b69e9de5c2f5703455199a
Suhaib Eltayeb <suhab.edu@gmail.com>
2024-12-10 22:42:49 +0200

    first commit

---
+++ il_env_backend_api/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for il_env_backend_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'il_env_backend_api.settings')
+
+application = get_wsgi_application()
